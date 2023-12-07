2181256acfeece18180ca4154f749b5d576b66c9
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-07 09:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let container = document.getElementById("container");
+function functionName(){
+    let name = arguments.callee.name;
+    alert(name);
+}
+
+function AccioJob(){
+    let name = arguments.callee.name;
+    alert(name);
+}
+// document.addEventListener("DOMContentLoaded",functionName);
+// document.addEventListener("DOMContentLoaded",AccioJob);
+functionName();
+AccioJob();
